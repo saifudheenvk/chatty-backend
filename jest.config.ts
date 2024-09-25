720e858528de1f2a45897adc85180b37a3c5b4ad
saifudheen <saifudheen@innovatise.com>
2024-09-25 17:34:48 +0530

    completed chats

--- jest.config.ts
+++ jest.config.ts
@@ -30,7 +30,7 @@ const config: Config = {
     '@follower/(.*)': ['<rootDir>/src/features/followers/$1'],
     '@notification/(.*)': ['<rootDir>/src/features/notifications/$1'],
     '@image/(.*)': ['<rootDir>/src/features/images/$1'],
-    '@chat/(.*)': ['<rootDir>/src/features/chat/$1'],
+    '@chat/(.*)': ['<rootDir>/src/features/chats/$1'],
     '@global/(.*)': ['<rootDir>/src/shared/globals/$1'],
     '@services/(.*)': ['<rootDir>/src/shared/services/$1'],
     '@sockets/(.*)': ['<rootDir>/src/shared/sockets/$1'],
